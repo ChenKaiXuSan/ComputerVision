0d26bd5d4b8b6e926b912df96aa15ac179c73f02
chenkaixu <chenkaixusan@gmail.com>
2020-02-17 18:55:48 +0900

    尝试pytorch

--- PyTorch/start.py
+++ PyTorch/start.py
@@ -1,5 +1,11 @@
 from __future__ import print_function
 import torch
+
+# 创建一个矩阵，未初始化
+y = torch.empty(5, 3)
+print(y)
+
+# 初始化一个矩阵
 x = torch.rand(5, 3)
 print(x)
 
@@ -7,3 +13,33 @@ import torch
 print(torch.cuda.is_available())
 
 print("hello")
+
+# 创建一个0填充的矩阵，数据类型为long:
+z = torch.zeros(5, 3, dtype=torch.long)
+print(z)
+
+# 创建tensor并使用现有数据初始化:
+w = torch.tensor([5.5, 3])
+print(w)
+
+x = x.new_ones(5, 3, dtype=torch.double)      # new_* 方法来创建对象
+print(x)
+
+x = torch.randn_like(x, dtype=torch.float)    # 覆盖 dtype!
+print(x)                                      #  对象的size 是相同的，只是值和类型发生了变化
+
+print(x.size())
+
+y = torch.rand(5, 3)
+print(x + y)
+
+print(x[:, 1])
+
+x = torch.randn(4, 4)
+y = x.view(16)
+z = x.view(-1, 8)  #  size -1 从其他维度推断
+print(x.size(), y.size(), z.size())
+
+x = torch.randn(1)
+print(x)
+print(x.item())
--- README.md
+++ README.md
@@ -2,5 +2,15 @@
 学习TensorFlow和PyTorch的过程记录
 
 ## 运行环境
-/home/chenkaixu/venv/bin/python3.6
 需要在虚拟环境中运行。
+```
+/home/chenkaixu/venv/bin/python3.6
+```
+
+### 在settings.json中设置运行环境
+```
+"python.pythonPath": "/home/chenkaixu/venv/bin/python3.6",
+"python.venvPath": "/home/chenkaixu/venv",
+"python.linting.pylintPath": "pylint",
+"python.formatting.autopep8Path": "autopep8"
+```
