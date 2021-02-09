7ae77a7f543d928d5cf42eeb0a2183582924c7dd
Xu <chenkaixusan@gmail.com>
2021-02-09 21:06:55 +0900

    update unet

--- PyTorch/GAN_implementatnion/unet_stylegan2/unet_stylegan2.py
+++ PyTorch/GAN_implementatnion/unet_stylegan2/unet_stylegan2.py
@@ -5,6 +5,7 @@ import math
 # import fire
 import json
 from torch.functional import split
+from torch.nn.modules.conv import Conv2d
 from tqdm import tqdm
 from math import floor, log2
 from random import random
@@ -24,7 +25,7 @@ from torch.autograd import grad as torch_grad
 import torchvision
 from torchvision import transforms
 
-from linear_attention_transformer import ImageLinearAttention
+from linear_attention_transformer import ImageLinearAttention, images
 
 from PIL import Image
 from pathlib import Path
@@ -272,5 +273,269 @@ def set_requires_grad(model, bool):
     for p in model.parameters():
         p.requires_grad = bool
 
+def slerp(val, low, high):
+    low_norm = low / torch.norm(low, dim=1, keepdim=True)
+    high_norm = high / torch.norm(high, dim=1, keepdim=True)
+    omega = torch.acos((low_norm * high_norm).sum(1))
+    so = torch.sin(omega)
+    res = (torch.sin((1.0 - val ) * omega) / so).unsqueeze(1) * low + (torch.sin(val * omega) / so).unsqueeze(1) * high
+    return res 
+
+def warmup(start, end, max_steps, current_step):
+    if current_step > max_steps:
+        return end
+    return  (end - start) * (current_step / max_steps) + start
+
+def log(t, eps = 1e-6):
+    return torch.log(t + eps)
+
+def cutmix_coordinates(height, width, alpha = 1.):
+    lam = np.random.beta(alpha, alpha)
+
+    cx = np.random.uniform(0, width)
+    cy = np.random.uniform(0, height)
+    w = width * np.sqrt(1 - lam)
+    h = height * np.sqrt(1 - lam)
+    x0 = int(np.round(max(cx - w / 2, 0)))
+    x1 = int(np.round(min(cx + w / 2, width)))    
+    y0 = int(np.round(max(cy - h / 2, 0)))
+    y1 = int(np.round(min(cy + h / 2, height)))
+
+    return ((y0, y1), (x0, x1)), lam
+
+def cutmix(source, target, coors, alpha = 1.):
+    source, target = map(torch.clone, (source, target))
+    ((y0, y1), (x0, x1)), _ = coors
+    source[:, :, y0:y1, x0:x1] = target[:, :, y0:y1, x0:x1]
+    return source
+
+def mask_src_tgt(source, target, mask):
+    return source * mask + (1 - mask) * target
 
 # %%
+# dataset 
+
+def convert_rgb_to_transparent(image):
+    if image.mode == 'RGB':
+        return image.convert('RGBA')
+    return image
+
+def convert_transparent_to_rgb(image):
+    if image.mode == 'RGBA':
+        return image.convert('RGB')
+    return image 
+
+class expand_greyscale(object):
+    def __init__(self, num_channels) -> None:
+        super().__init__()
+        self.num_chanels = num_channels
+    
+    def __call__(self, tensor):
+        return tensor.expand(self.num_chanels, -1, -1)
+
+
+def resize_to_minimum_size(min_size, image):
+    if max(*image.size) < min_size:
+        return torchvision.transforms.functional.resize(image, min_size)
+    return image 
+
+class Dataset(data.Dataset):
+    def __init__(self, folder, image_size, transparent = False, aug_prob = 0.) -> None:
+        super().__init__()
+        self.folder = folder
+        self.image_size = image_size
+        self.paths = [p for ext in EXTS for p in Path(f'{folder}').glob(f'**/*.{ext}')]
+
+        convert_image_fn = convert_transparent_to_rgb if not transparent else convert_rgb_to_transparent
+        num_channels = 3 if not transparent else 4
+
+        self.transform = transforms.Compose({
+            transforms.Lambda(convert_image_fn),
+            transforms.Lambda(partial(resize_to_minimum_size, image_size)),
+            transforms.Resize(image_size),
+            RandomApply(aug_prob, transforms.RandomResizedCrop(image_size, scale=(0.5, 1.0), 
+                        ratio=(0.98, 1.02)), transforms.CenterCrop(image_size)),
+            transforms.ToTensor(),
+            transforms.Lambda(expand_greyscale(num_channels)),
+        })
+        
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, index):
+        path = self.paths[index]
+        img = Image.open(path)
+        return self.transform(img)
+        
+# %%
+# augmentations
+
+def random_float(lo, hi):
+    return lo + (hi - lo) * random()
+
+def random_crop_and_resize(tensor, scale):
+    b, c, h, _ = tensor.shape
+    new_width = int(h * scale)
+    delta = h - new_width
+    h_delta = int(random() * delta)
+    w_delta = int(random() * delta)
+    cropped = tensor[:, :, h_delta:(h_delta + new_width), w_delta:(w_delta + new_width)].clone()
+    return F.interpolate(cropped, size=(h, h), mode='bilinear')
+
+def random_hflip(tensor, prob):
+    if prob > random():
+        return tensor
+    return torch.flip(tensor, dim=(3, ))
+
+class AugWrapper(nn.Module):
+    def __init__(self, D, image_size, types):
+        super().__init__()
+        self.D = D
+        self.types = types 
+
+    def forward(self, images, prob=0., detach = False):
+        if random() < prob:
+            images = random_hflip(images, prob=0.5)
+            images = DiffAugment(images, types= self.types)
+
+        if detach:
+            images.detach_()
+
+        return self.D(images), images
+
+# %%
+# stylegan2 classes
+
+class EqualLinear(nn.Module):
+    def __init__(self, in_dim, out_dim, lr_mul = 1, bias = True):
+        super().__init__()
+        self.weight = nn.Parameter(torch.randn(out_dim, in_dim))
+        if bias:
+            self.bias = nn.Parameter(torch.zeros(out_dim))
+
+        self.lr_mul = lr_mul
+
+    def forward(self, input):
+        return F.linear(input, self.weight * self.lr_mul, bias = self.bias * self.lr_mul)
+
+class StyleVectorizer(nn.Module):
+    def __init__(self, emb, depth, lr_mul = 0.1):
+        super().__init__()
+
+        layers = []
+        for i in range(depth):
+            layers.extend([EqualLinear(emb, emb, lr_mul), leaky_relu()])
+    
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = F.normalize(x, dim=1)
+        return self.net(x)
+
+class Conv2DMod(nn.Module):
+    def __init__(self, in_chan, out_chan, kernel, demod = True, stride = 1, dilation = 1, **kwargs):
+        super().__init__()
+        self.filters = out_chan
+        self.demod = demod
+        self.kernel = kernel 
+        self.stride = stride
+        self.dilation = dilation
+        self.weight = nn.Parameter(torch.randn((out_chan, in_chan, kernel, kernel)))
+        nn.init.kaiming_normal_(self.weight, a=0, mode='fan_in', nonlinearity='leaky_relu')
+
+    def _get_same_padding(self, size, kernel, dilation, stride):
+        return ((size - 1) * (stride - 1) + dilation * (kernel - 1)) // 2
+
+    def forward(self, x, y):
+        b, c, h, w = x.shape
+
+        w1 = y[:, None, :, None, None]
+        w2 = self.weight[None, :, :, :, :]
+        weights = w2 * (w1 + 1)
+
+        if self.demod:
+            d = torch.rsqrt((weights ** 2).sum(dim=(2, 3, 4), keepdim=True) + EPS)
+            weights = weights * d
+        
+        x = x.reshape(1, -1, h, w)
+
+        _, _, *ws = weights.shape
+        weights = weights.reshape(b * self.filters, *ws)
+
+        padding = self._get_same_padding(h, self.kernel, self.dilation, self.stride)
+        x = F.conv2d(x, weights, padding=padding, groups=b)
+
+        x = x.reshape(-1, self.filters, h, w)
+        return x
+
+class RGBBlock(nn.Module):
+    def __init__(self, latent_dim, input_channel, upsample, rgba = False):
+        super().__init__()
+        self.input_channel = input_channel
+        self.to_style = nn.Linear(latent_dim, input_channel)
+
+        out_filters = 3 if not rgba else 4
+        self.conv = Conv2DMod(input_channel, out_filters, 1, demod=False)
+
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False) if upsample else None
+
+    def forward(self, x, prev_rgb, istyle):
+        b, c, h, w = x.shape
+        style = self.to_style(istyle)
+        x = self.conv(x, style)
+
+        if prev_rgb is not None:
+            x = x + prev_rgb
+
+        if self.upsample is not None:
+            x = self.upsample(x)
+
+        return x 
+
+class GeneratorBlock(nn.Module):
+    def __init__(self, latent_dim, input_channels, filters, upsample = True, upsample_rgb = True, rgba = False):
+        super().__init__()
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False) if upsample else None
+
+        self.to_style1 = nn.Linear(latent_dim, input_channels)
+        self.to_noise1 = nn.Linear(1, filters)
+        self.conv1 = Conv2DMod(input_channels, filters, 3)
+
+        self.to_style2 = nn.Linear(latent_dim, filters)
+        self.to_noise2 = nn.Linear(1, filters)
+        self.conv2 = Conv2DMod(filters, filters, 3)
+
+        self.activation = leaky_relu()
+        self.to_rgb = RGBBlock(latent_dim, filters, upsample_rgb, rgba)
+
+    def forward(self, x, prev_rgb, istyle, inoise):
+        if self.upsample is not None:
+            x = self.upsample(x)
+
+        inoise = inoise[:, :x.shape[2], :x.shape[3], :]
+        noise1 = self.to_noise1(inoise).permute((0, 3, 2, 1))
+        noise2 = self.to_noise2(inoise).permute((0, 3, 2, 1))
+
+        style1 = self.to_style1(istyle)
+        x = self.conv1(x, style1)
+        x = self.activation(x + noise1)
+
+        style2 = self.to_style2(istyle)
+        x = self.conv2(x, style2)
+        x = self.activation(x + noise2)
+
+        rgb = self.to_rgb(x, prev_rgb, istyle)
+        return x, rgb
+
+def double_conv(chan_in, chan_out):
+    return nn.Sequential(
+        nn.Conv2d(chan_in, chan_out, 3, padding=1),
+        leaky_relu(),
+        nn.Conv2d(chan_out, chan_out, 3, padding=1),
+        leaky_relu()
+    )
+
+
+
+
+        # %%
