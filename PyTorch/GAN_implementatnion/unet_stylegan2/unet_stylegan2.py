d36020d7e32a655f09b58d8c88afb62467fe4ba3
Xu <chenkaixusan@gmail.com>
2021-02-10 22:23:39 +0900

    update

--- PyTorch/GAN_implementatnion/unet_stylegan2/unet_stylegan2.py
+++ PyTorch/GAN_implementatnion/unet_stylegan2/unet_stylegan2.py
@@ -528,6 +528,16 @@ class GeneratorBlock(nn.Module):
         return x, rgb
 
 def double_conv(chan_in, chan_out):
+    '''
+    unet结构
+
+    Args:
+        chan_in (tensor): [description]
+        chan_out (tensor): [description]
+
+    Returns:
+        nn.Sequential: conv2d, leakyrelu, conv2d, leakyrelu
+    '''    
     return nn.Sequential(
         nn.Conv2d(chan_in, chan_out, 3, padding=1),
         leaky_relu(),
@@ -535,7 +545,158 @@ def double_conv(chan_in, chan_out):
         leaky_relu()
     )
 
+class DownBlock(nn.Module):
+    def __init__(self, input_channels, filters, downsample=True):
+        super().__init__()
+        self.conv_res = nn.Conv2d(input_channels, filters, 1, stride=(2 if downsample else 1))
 
+        self.net = double_conv(input_channels, filters)
+        self.down = nn.Conv2d(filters, filters, 3, padding=1, stride=2) if downsample else None
 
+    def forward(self, x):
+        res = self.conv_res(x)
+        x = self.net(x)
+        unet_res = x
 
+        if self.down is not None:
+            x = self.down(x)
+
+        x = x + res
+        return x, unet_res
+
+class UpBlock(nn.Module):
+    def __init__(self, input_channels, filters):
+        super().__init__()
+        self.conv_res = nn.ConvTranspose2d(input_channels // 2, filters, 1, stride=2)
+        self.net = double_conv(input_channels, filters)
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
+        self.input_channels = input_channels
+        self.filters = filters
+
+    def forward(self, x, res):
+        *_, h, w = x.shape
+        conv_res = self.conv_res(x, output_size = (h * 2, w * 2))
+        x = self.up(x)
+        x = torch.cat((x, res), dim=1)
+        x = self.net(x)
+        x = x + conv_res
+        return x 
+
+class Generator(nn.Module):
+    def __init__(self, image_size, latent_dim, network_capacity = 16, transparent=False, no_const = False, fmap_max = 512):
+        super().__init__()
+        self.image_size = image_size
+        self.latent_dim = latent_dim
+        self.num_layers = int(log2(image_size) - 1)
+
+        filters = [network_capacity * 2 (2 ** (i + 1)) for i in range(self.num_layers)][::-1]
+
+        set_fmap_max = partial(min, fmap_max)
+        filters = list(map(set_fmap_max, filters))
+        init_channels = filters[0]
+        filters = [init_channels, *filters]
+
+        in_out_pairs =  zip(filters[:-1], filters[1:])
+        self.no_const = no_const
+
+        if no_const:
+            self.to_initaial_block = nn.ConvTranspose2d(latent_dim, init_channels, 4, 1, 0, bias=False)
+        else:
+            self.initaial_block = nn.Parameter(torch.randn((1, init_channels, 4, 4)))
+        
+        self.initial_conv = nn.Conv2d(filters[0], filters[0], 3, padding=1)
+
+        self.blocks = nn.ModuleList([])
+        self.attns = nn.ModuleList([])
+
+        for ind, (in_chan, out_chan) in enumerate(in_out_pairs):
+            not_first = ind != 0
+            not_last = ind != (self.num_layers - 1)
+            num_layer = self.num_layers - ind
+
+            attn_fn = attn_and_ff(in_chan)
+            self.attns.append(attn_fn)
+
+            block = GeneratorBlock(
+                latent_dim,
+                in_chan,
+                out_chan,
+                upsample=not_first,
+                upsample_rgb=not_last,
+                rgba=transparent,
+            )
+
+            self.blocks.append(block)
+
+    def forward(self, styles, input_noise):
+        batch_size = styles.shape[0]
+        image_size = self.image_size
+
+        if self.no_const:
+            avg_style = styles.mean(dim=1)[:, :, None, None]
+            x = self.to_initaial_block(avg_style)
+        else:
+            x = self.initaial_block.expand(batch_size, -1, -1, -1)
+        
+        x = self.initial_conv(x)
+        styles = styles.transpose(0,1)
+
+        rgb = None 
+        for style, block, attn in zip(styles, self.blocks, self.attns):
+            if attn is not None:
+                x = attn(x)
+            
+            x, rgb = block(x, rgb, style, input_noise)
+
+        return rgb
+
+# %%
+class Discriminator(nn.Module):
+    def __init__(self, image_size, network_capacity = 16, transparent = False, fmap_max = 512):
+        super().__init__()
+        num_layers = int(log2(image_size) - 3)
+        num_init_filters = 3 if not transparent else 4
+
+        blocks = []
+        filters = [num_init_filters] + [(network_capacity) * (2 ** i) for i in range(num_layers + 1)]
+
+        set_fmap_max = partial(min, fmap_max)
+        filters = list(map(set_fmap_max, filters))
+        filters[-1] = filters[-2]
+
+        chan_in_out = list(zip(filters[:-1], filters[1:]))
+        chan_in_out = list(map(list, chan_in_out))
+
+        down_blocks = []
+        attn_blocks = []
+
+        for ind, (in_chan, out_chan) in enumerate(chan_in_out):
+            num_layer = ind + 1
+            is_not_last = ind != (len(chan_in_out) - 1)
+
+            block = DownBlock(in_chan, out_chan, downsample=is_not_last)
+            down_blocks.append(block)
+
+            attn_fn = attn_and_ff(out_chan)
+            attn_blocks.append(attn_fn)
+
+        self.down_blocks = nn.ModuleList(down_blocks)
+        self.attn_blocks = nn.ModuleList(attn_blocks)
+
+        last_chan = filters[-1]
+
+        self.to_logit = nn.Sequential(
+            leaky_relu(),
+            nn.AvgPool2d(image_size // (2 ** num_layers)),
+            Flatten(1),
+            nn.Linear(last_chan, 1)
+        )
+
+        self.conv = double_conv(last_chan, last_chan)
+
+        dec_chan_in_out = chan_in_out[:-1][::-1]
+        self.up_blocks = nn.ModuleList(list(map(lambda c: UpBlock(c[1] * 2, c[0]), dec_chan_in_out)))
+        self.conv_out = nn.Conv2d(3, 1, 1)
+
+        
         # %%
