95d5684002f63be2a28b78c0de155124b9f2bc9a
chenkaixu@lab <chenkaixusan@gmail.com>
2021-03-05 13:35:10 +0900

    select gpu

--- PyTorch/GAN_implementatnion/w_dcgan/w_dcgan_mnist.py
+++ PyTorch/GAN_implementatnion/w_dcgan/w_dcgan_mnist.py
@@ -18,6 +18,9 @@ import torch
 from torch.utils.tensorboard import SummaryWriter
 
 os.makedirs("images", exist_ok=True)
+
+# 设置gpu
+os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 # %%
 # 设置参数
 parser = argparse.ArgumentParser()
